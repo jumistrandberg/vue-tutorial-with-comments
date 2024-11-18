e37766ceb28f6dcb00a978910fbe90b9322022f8
Jumi <feliciastrandberg@gmail.com>
2024-11-18 18:26:45 +0100

    Adds comment on nesting

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 <template>
+  <!-- The App component is the parent component of all other components. -->
   <HelloWorld />
 </template>
 <style scoped></style>
