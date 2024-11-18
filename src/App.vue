17f394de6e02dd7320d71127f13590cf56ba50d4
Jumi <feliciastrandberg@gmail.com>
2024-11-18 18:40:56 +0100

    Creates and imports component for attribute bindings

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 // Import inside script setup
 import HelloWorld from "./components/HelloWorld.vue";
 import MyComponent from "./components/MyComponent.vue";
+import AttributeBindings from "./components/AttributeBindings.vue";
 </script>
 
 <template>
   <!-- The App component is the parent component of all other components. -->
   <HelloWorld />
   <MyComponent />
+  <AttributeBindings />
 </template>
 
 <style scoped></style>
