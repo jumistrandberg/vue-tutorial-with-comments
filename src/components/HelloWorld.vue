102c78405e6bd44221a4e9cd9e7c0de3a9f56620
Jumi <feliciastrandberg@gmail.com>
2024-11-18 18:14:51 +0100

    Adds first style and script

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,21 @@
 <!-- Recommended to use multi-word names for components  -->
-<!-- HTML -->
- <!-- template tag to write html  -->
- <template>
-    <h1>Hello World</h1>
- </template>
-
+<!-- Not required but many write js, html then css  -->
  
+<!-- JS -->
+<script setup>
+console.log("Hello World");
+</script>
+
+<!-- HTML -->
+<!-- template tag to write html  -->
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<!-- CSS -->
+<style>
+h1 {
+  color: red;
+}
+</style>
+
