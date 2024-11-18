889756a693f06f364d2e845be777d41e37d7e531
Jumi <feliciastrandberg@gmail.com>
2024-11-18 18:31:38 +0100

    Adds component to show text interpolation and imports it in App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 // Import inside script setup
 import HelloWorld from "./components/HelloWorld.vue";
+import MyComponent from "./components/MyComponent.vue";
 </script>
+
 <template>
   <!-- The App component is the parent component of all other components. -->
   <HelloWorld />
+  <MyComponent />
 </template>
+
 <style scoped></style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,8 @@
+<script setup>
+const myMessage = "Hej I am come from a js const hehe";
+</script>
+
+<template>
+    <!-- Syntax for text interpolation, how to extract data from js to html dynamically -->
+    <h2>{{ myMessage }}</h2>   
+</template>
